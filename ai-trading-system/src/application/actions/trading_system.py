46d724f11a8099462cf3742105fe172a2f644e9f
ruromgar <ruromgar@gmail.com>
2021-01-14 23:30:46 +0100

    integration with Alpaca

--- ai-trading-system/src/application/actions/trading_system.py
+++ ai-trading-system/src/application/actions/trading_system.py
@@ -20,20 +20,5 @@ class TradingSystem:
             await asyncio.sleep(seconds)
 
         while True:
-            self._alpaca_repository.test()
-            await asyncio.sleep(seconds)
-
-    async def monitoring_bak(self, seconds, exec_on_start):
-        if not exec_on_start:
-            await asyncio.sleep(seconds)
-
-        while True:
-            finance_data = self._yahoo_repository.get_finance_data()
-
-            if finance_data is None:
-                self._logger.info(f'No new movements')
-            else:
-                last_real_data, forecast = self._ai_repository.get_forecast(finance_data)
-                self._logger.info(f'Last data: {last_real_data} | Forecast: {forecast}')                    
-
+            self._alpaca_repository.is_market_open()
             await asyncio.sleep(seconds)
